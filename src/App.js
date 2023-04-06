925b3295e8432e174034bf59a5de7a904ac60663
Kacper Góźdź <kacpah@hotmail.com>
2023-04-06 17:16:57 +0200

    part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+const Header = (params) => {
+  return (
+    <h1>{params.course}</h1>
+  )
+}
+
+const Content = (params) => {
+  return (
+    <>
+    <p>{params.part1} {params.exercises1}</p>
+    <p>{params.part2} {params.exercises2}</p>
+    <p>{params.part3} {params.exercises3}</p>
+  </>
+  )
+}
+
+const Total = (params) => {
+  return (
+    <p>Number of exercises {params.exercises1 + params.exercises2 + params.exercises3}</p>
+  )
+}
